538be7fcbdf2556f218f71b765781187267b0f10
Pablo Henrique Vieira de Nadai <pablo.nadai@etec.sp.gov.br>
2021-09-21 21:27:22 -0300

    V:1.1 - Integração PHP

---
+++ page1.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>Welcome!</title>
+    </head>
+    <body>
+        <section>
+            <p>Bem vindo!</p>
+            <p>Primeiramente selecione a grandeza qual você quer converter:</p>
+            <div>
+                <!--Div para escolher grand-->
+                <button onclick="">Tensão (Volts - V)</button>
+                <button onclick="">Resistência (Ohms - R)</button>
+                <button onclick="">Potencia (Watts - P)</button>
+                <button onclick="">Corrente (Amperes - A)</button>
+            </div>
+        </section>
+        <?php
+            echo 'olá';
+        ?>
+    </body>
+</html>
