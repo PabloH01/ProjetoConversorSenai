8d67decf2b8d96d8f4955c91ba64fb411becc83d
Pablo Henrique Vieira de Nadai <pablo.nadai@etec.sp.gov.br>
2021-09-22 00:32:38 -0300

    V2.0 - Conversão Volt Feita!

--- page1.php
+++ page1.php
@@ -1,21 +1,30 @@
+<!DOCTYPE html>
+<meta charset="utf-8"> 
 <html>
     <head>
+        <head lang="pt-br">
         <title>Welcome!</title>
     </head>
     <body>
-        <section>
-            <p>Bem vindo!</p>
-            <p>Primeiramente selecione a grandeza qual você quer converter:</p>
-            <div>
-                <!--Div para escolher grand-->
-                <button onclick="">Tensão (Volts - V)</button>
-                <button onclick="">Resistência (Ohms - R)</button>
-                <button onclick="">Potencia (Watts - P)</button>
-                <button onclick="">Corrente (Amperes - A)</button>
-            </div>
-        </section>
-        <?php
-            echo 'olá';
-        ?>
+    <script type="text/javascript"></script>
+    <section>
+        <p>Bem vindo!</p>
+        <p>Primeiramente selecione a grandeza qual você quer converter:</p>
+        <div>
+            <!--Div para escolher grand-->
+            <button onclick="window.location.href='../ProjetoConversorSenai/calcularTensao.php'">Tensão (Volts - V)</button>
+            <button onclick="">Resistência (Ohms - R)</button>
+            <button onclick="">Potencia (Watts - P)</button>
+            <button onclick="">Corrente (Amperes - A)</button>
+        </div>
+    </section>
     </body>
 </html>
+<!-- Utilização dos recursos -->
+<?php
+    function calcularTensao()
+    {
+        echo "<label>Digite o primeiro valor:</label>";
+    }
+?>
+
